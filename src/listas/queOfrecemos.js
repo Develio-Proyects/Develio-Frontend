dd11ee37a5a96f338552e5255693d98664903666
tobiasriccone <tobi_riccone@hotmail.com>
2026-03-02 22:46:22 -0300

    Update image paths to relative URLs and set homepage in package.json

--- src/listas/queOfrecemos.js
+++ src/listas/queOfrecemos.js
@@ -3,7 +3,7 @@ export const queOfrecemos = [
         id: 1,
         title: " Inteligencia Artificial aplicada",
         description: "Incorporamos IA para optimizar procesos, mejorar la toma de decisiones y potenciar la experiencia del usuario.",
-        image: '/ia.png',
+        image: './ia.png',
         float1: 'float 1', 
         float2: 'float2'
     },
@@ -11,7 +11,7 @@ export const queOfrecemos = [
         id: 2,
         title: "Rendimiento y escalabilidad",
         description: "Desarrollamos software con tecnologías avanzadas, asegurando rapidez, seguridad y crecimiento sin límites.",
-        image: '/escalabilidad.png',
+        image: './escalabilidad.png',
         float1: 'float 1', 
         float2: 'float2'
     },
@@ -19,7 +19,7 @@ export const queOfrecemos = [
         id: 3,
         title: "Experiencia centrada en el usuario",
         description: "Creamos interfaces intuitivas y eficientes, priorizando la usabilidad y accesibilidad en cada proyecto.",
-        image: '/uxui.png',
+        image: './uxui.png',
         float1: 'float 1', 
         float2: 'float2'
     }
